3012650271f0c9f8b0b478b03fc97a85211b4952
Winifred Frierson <winifred.dykes@hotmail.com>
2020-01-15 08:16:17 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -15,8 +15,11 @@ class Person
   end
   
   class PartnerError < StandardError
-  
-end
+    def message
+      "you must give the get_married method an argument of an instance of the person class!"
+    end
+  end
+
 
 end
 
