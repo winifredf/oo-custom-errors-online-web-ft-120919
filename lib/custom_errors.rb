3bf392402928a364bf73e45b0ad53984f228e0fc
Winifred Frierson <winifred.dykes@hotmail.com>
2020-01-15 08:19:27 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -1,10 +1,10 @@
 class Person
   attr_accessor :partner, :name
-
+ 
   def initialize(name)
     @name = name
   end
-
+ 
   def get_married(person)
     self.partner = person
     if person.class != Person
@@ -17,20 +17,14 @@ class Person
       person.partner = self
     end
   end
-  
+ 
   class PartnerError < StandardError
     def message
       "you must give the get_married method an argument of an instance of the person class!"
     end
   end
-
-
 end
-
+ 
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
-
-
-
-
